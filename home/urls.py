96d4f906ae4447b12ab480293c4f473a5a2118ec
fenisha <fenishaprajapti@gmail.com>
2023-12-28 02:03:32 +0530

    Initial Commit

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('home/', views.home, name="home"),
+    path('', views.home, name="home"),
     path('login/', views.login_user, name="login"),
     path('logout/', views.logout_user, name="logout"),
     path('register/', views.register_user, name="register"),
